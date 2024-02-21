ba8ec8d620f13876a3e8b9ae235a49c0d4af1342
Georgi Genov <123077056+G05h0@users.noreply.github.com>
2024-02-21 21:56:06 +0200

    Delete 09. Fruit.js

--- 09. Fruit.js
+++
@@ -1,5 +0,0 @@
-function solve(fruit, weight, pricePerKg) {
-    let money = weight * pricePerKg;
-
-    console.log(`I need ${money.toFixed(2)} to buy ${weight.toFixed(2)} kilograms ${fruit}.`);
-}
