8ad01617fdc70e2b0f84b026f4c6865c897da5d2
Georgi Genov <123077056+G05h0@users.noreply.github.com>
2024-03-08 16:53:55 +0200

    Add files via upload

---
+++ 05. Functions and Statements/06. Sign Check.js
@@ -0,0 +1,9 @@
+function solve(a, b, c) {
+    const multipy = (a, b) => a * b;
+
+    if (multipy(multipy(a, b), c) >= 0) {
+        console.log('Positive');
+    } else {
+        console.log('Negative');
+    }
+}
